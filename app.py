c87bf05954a34d80517d81d5d6b34f1bedefd738
Monica Zuñiga <momazumu1996@gmail.com>
2026-02-21 16:40:09 -0500

    version 3

--- app.py
+++ app.py
@@ -1,33 +1,30 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-# -------------------------
-#   RUTA PRINCIPAL
-# -------------------------
-@app.route('/')
-def inicio():
-    return render_template("index.html")
-
-# -------------------------
-#   RUTA DE PRODUCTOS
-# -------------------------
-@app.route('/productos')
-def productos():
-    lista_productos = [
-        {"nombre": "Boli de Fresa", "precio": 1.00, "imagen": "fresa.png"},
-        {"nombre": "Boli de Mango", "precio": 1.00, "imagen": "mango.jpg"},
-        {"nombre": "Boli de Oreo", "precio": 1.50, "imagen": "oreo.png"},
-        {"nombre": "Boli de Coco", "precio": 1.00, "imagen": "coco.jpg"},
-        {"nombre": "Boli de Mora", "precio": 1.00, "imagen": "mora.png"},
-    ]
-    return render_template("productos.html", productos=lista_productos)
-# -------------------------
-#   RUTA DINÁMICA
-# -------------------------
-@app.route('/producto/<nombre>')
-def producto(nombre):
-    return render_template("detalle_producto.html", nombre=nombre)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+lista_productos = [
+    {
+        "nombre": "Boli de Fresa",
+        "descripcion": "Delicioso boli de fresa natural.",
+        "precio": 1.00,
+        "imagen": "/static/img/fresa.jpg"
+    },
+    {
+        "nombre": "Boli de Mango",
+        "descripcion": "Refrescante boli de mango maduro.",
+        "precio": 1.00,
+        "imagen": "/static/img/mango.jpg"
+    },
+    {
+        "nombre": "Boli de coco",
+        "descripcion": "Boli hecho con coco fresco.",
+        "precio": 1.00,
+        "imagen": "/static/img/sandia.jpg"
+    },
+    {"nombre": "Boli de mora",
+        "descripcion": "Boli hecho con mora sacado de la plata a la olla.",
+        "precio": 1.00,
+        "imagen": "/static/img/mora.jpg"
+    },
+    {"nombre": "Boli de oreo",
+        "descripcion": "Boli hecho con la mejor galleta de oreo.",
+        "precio": 1.00,
+        "imagen": "/static/img/sandia.jpg"
+    }
+]
