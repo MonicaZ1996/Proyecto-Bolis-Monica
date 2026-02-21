246511be1610c445fbc2bb34a85a9a62d24798db
Monica Zuñiga <momazumu1996@gmail.com>
2026-02-21 09:50:15 -0500

    correccion semana 10

--- app.py
+++ app.py
@@ -2,49 +2,33 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# ----------------------------
-# RUTA PRINCIPAL
-# ----------------------------
+# -------------------------
+#   RUTA PRINCIPAL
+# -------------------------
 @app.route('/')
 def inicio():
-    return render_template('index.html')
+    return render_template("index.html")
 
-# ----------------------------
-# RUTA ACERCA DE
-# ----------------------------
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-# ----------------------------
-# RUTA PRODUCTOS
-# ----------------------------
+# -------------------------
+#   RUTA DE PRODUCTOS
+# -------------------------
 @app.route('/productos')
 def productos():
     lista_productos = [
-        {"nombre": "Boli de Fresa", "precio": 0.50},
-        {"nombre": "Boli de Mango", "precio": 0.60},
-        {"nombre": "Boli de Oreo", "precio": 0.90},
-        {"nombre": "Boli de Maracuyá", "precio": 0.70},
+        {"nombre": "Boli de Fresa", "precio": 1.00},
+        {"nombre": "Boli de Mango", "precio": 1.00},
+        {"nombre": "Boli de Oreo", "precio": 1.50},
+        {"nombre": "Boli de Coco", "precio": 1.00},
+        {"nombre": "Boli de Mora", "precio": 1.00},
     ]
-    return render_template('productos.html', productos=lista_productos)
+    return render_template("productos.html", productos=lista_productos)
 
-# ----------------------------
-# RUTA CLIENTE DINÁMICA
-# ----------------------------
-@app.route('/cliente/<nombre>')
-def cliente(nombre):
-    return render_template('cliente.html', nombre=nombre)
+# -------------------------
+#   RUTA DINÁMICA
+# -------------------------
+@app.route('/producto/<nombre>')
+def producto(nombre):
+    return render_template("detalle_producto.html", nombre=nombre)
 
-# ----------------------------
-# RUTA FACTURA
-# ----------------------------
-@app.route('/factura/<producto>')
-def factura(producto):
-    return render_template('factura.html', producto=producto)
-
-# ----------------------------
-# EJECUCIÓN LOCAL
-# ----------------------------
 if __name__ == '__main__':
     app.run(debug=True)
