e4f2692b9d5e807f1696253fe0223bb6a7460f01
Monica Zuñiga <momazumu1996@gmail.com>
2026-04-03 16:39:42 -0500

    proyecto 1

--- form.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class FormProducto:
-    nombre: str
-    precio: float
-    cantidad: int
---
+++ forms/producto_form.py
@@ -0,0 +1,54 @@
+from conexion.conexion import get_connection
+
+def obtener_productos():
+    conn = get_connection()
+    cursor = conn.cursor(dictionary=True)
+
+    cursor.execute("SELECT * FROM productos")
+    datos = cursor.fetchall()
+
+    conn.close()
+    return datos
+
+
+def insertar_producto(nombre, precio, stock):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    sql = "INSERT INTO productos (nombre, precio, stock) VALUES (%s, %s, %s)"
+    cursor.execute(sql, (nombre, precio, stock))
+
+    conn.commit()
+    conn.close()
+
+
+def obtener_producto(id):
+    conn = get_connection()
+    cursor = conn.cursor(dictionary=True)
+
+    cursor.execute("SELECT * FROM productos WHERE id_producto=%s", (id,))
+    dato = cursor.fetchone()
+
+    conn.close()
+    return dato
+
+
+def actualizar_producto(id, nombre, precio, stock):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    sql = "UPDATE productos SET nombre=%s, precio=%s, stock=%s WHERE id_producto=%s"
+    cursor.execute(sql, (nombre, precio, stock, id))
+
+    conn.commit()
+    conn.close()
+
+
+def eliminar_producto(id):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("DELETE FROM productos WHERE id_producto=%s", (id,))
+    conn.commit()
+
+    conn.close()
