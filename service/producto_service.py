1f667d9ec84d420c712fe8fa13e8393ecc7d91f8
Monica Zuñiga <momazumu1996@gmail.com>
2026-04-03 18:01:09 -0500

    correccion2

--- service/producto_service.py
+++ service/producto_service.py
@@ -4,7 +4,7 @@ def obtener_productos():
     conn = get_connection()
     cursor = conn.cursor(dictionary=True)
 
-    cursor.execute("SELECT * FROM productos")
+    cursor.execute("SELECT * FROM producto")
     datos = cursor.fetchall()
 
     conn.close()
@@ -15,7 +15,7 @@ def insertar_producto(nombre, precio, stock):
     conn = get_connection()
     cursor = conn.cursor()
 
-    sql = "INSERT INTO productos (nombre, precio, stock) VALUES (%s, %s, %s)"
+    sql = "INSERT INTO producto (nombre, precio, stock) VALUES (%s, %s, %s)"
     cursor.execute(sql, (nombre, precio, stock))
 
     conn.commit()
@@ -26,7 +26,7 @@ def obtener_producto(id):
     conn = get_connection()
     cursor = conn.cursor(dictionary=True)
 
-    cursor.execute("SELECT * FROM productos WHERE id_producto=%s", (id,))
+    cursor.execute("SELECT * FROM producto WHERE id_producto=%s", (id,))
     dato = cursor.fetchone()
 
     conn.close()
@@ -37,7 +37,7 @@ def actualizar_producto(id, nombre, precio, stock):
     conn = get_connection()
     cursor = conn.cursor()
 
-    sql = "UPDATE productos SET nombre=%s, precio=%s, stock=%s WHERE id_producto=%s"
+    sql = "UPDATE producto SET nombre=%s, precio=%s, stock=%s WHERE id_producto=%s"
     cursor.execute(sql, (nombre, precio, stock, id))
 
     conn.commit()
@@ -48,7 +48,7 @@ def eliminar_producto(id):
     conn = get_connection()
     cursor = conn.cursor()
 
-    cursor.execute("DELETE FROM productos WHERE id_producto=%s", (id,))
+    cursor.execute("DELETE FROM producto WHERE id_producto=%s", (id,))
     conn.commit()
 
     conn.close()
